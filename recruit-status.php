b41b1986d4d902f16816d847031c924ed960c917
Cryofrostt <benzaaa35@hotmail.com>
2018-05-20 15:43:35 +0700

    เย้

--- recruit-status.php
+++ recruit-status.php
@@ -2,8 +2,7 @@
 header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json; charset=UTF-8");
 
-$conn = new mysqli("127.0.0.1", "root", "", "projectdb");
-mysqli_query( $conn, "SET NAMES UTF8");
+include "dblink.php";
 $result = $conn->query("SELECT r.RecruitID, r.Prefix, r.FirstName, r.LastName, r.RecruitPlanName, r.SchoolID, r.SchoolGPAX, r.MobileNumber, r.Status, s.SchoolName FROM recruitinfo r, schoolinfo s WHERE r.SchoolID=s.SchoolID AND RecruitID = '".$_GET['inID']."';");
 
 $outp = "[";
