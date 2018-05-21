c2cf2576165b059bc4528cb70c8832452f6cf0e2
Cryofrostt <benzaaa35@hotmail.com>
2018-05-21 15:23:10 +0700

    งืมๆ

--- recruit-status-confirm-link.php
+++ recruit-status-confirm-link.php
@@ -1,8 +1,8 @@
 <?php
 include "dblink.php";
-$result = $conn->query("SELECT r.RecruitID, r.Prefix, r.FirstName, r.LastName, r.RecruitPlanName, b.BillRecruitID, b.DateRegister, b.AcademicYear
-                        FROM recruitinfo r,  billrecruit b
-                        WHERE r.RecruitID=b.RecruitID AND r.RecruitID = '".$_GET['inID']."';");
+$result = $conn->query("SELECT r.RecruitID, r.Prefix, r.FirstName, r.LastName, r.RecruitPlanName, b.BillRecruitID, b.DateRegister, b.AcademicYear, r.Department, d.Faculty
+                        FROM recruitinfo r,  billrecruit b, departmentinfo d
+                        WHERE r.RecruitID=b.RecruitID AND r.Department=d.Department AND r.RecruitID = '".$_GET['inID']."';");
 
 $outp = "[";
 while($rs = $result->fetch_array(MYSQLI_ASSOC)) {
@@ -14,6 +14,8 @@ while($rs = $result->fetch_array(MYSQLI_ASSOC)) {
     $outp .= '"RecruitPlanName":"'.$rs["RecruitPlanName"].'",';
     $outp .= '"BillRecruitID":"'.$rs["BillRecruitID"].'",';
     $outp .= '"DateRegister":"'.$rs["DateRegister"].'",';
+    $outp .= '"Department":"'.$rs["Department"].'",';
+    $outp .= '"Faculty":"'.$rs["Faculty"].'",';
     $outp .= '"AcademicYear":"'.$rs["AcademicYear"].'"}';
 }
 $outp .="]";
--- recruit-status-reject-link.php
+++ recruit-status-reject-link.php
@@ -1,7 +1,7 @@
 <?php
 include "dblink.php";
 $result = $conn->query("UPDATE recruitinfo
-                        SET Status='สละสิทธิ์', MovedUniversityName='".$_GET['inUni']."'
+                        SET Status='สละสิทธิ์', MovedUniversityName='".$_POST['inUni']."'
                         WHERE RecruitID='".$_GET['inID']."';");
 
 $conn->close();
