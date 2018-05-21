1e84b81eac9fa7b5d609872268f9f787319cb993
darknessdearm <38848518+darknessdearm@users.noreply.github.com>
2018-05-21 12:04:44 +0700

    update

--- student-new-login.php
+++ student-new-login.php
@@ -91,6 +91,7 @@
                 color: white;
             }
             input[type=submit] {
+                font-family: "supermarket";
                 padding: 5px;
                 width: 80px;
                 margin-top: 25px;
