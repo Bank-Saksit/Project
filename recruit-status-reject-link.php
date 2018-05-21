7f2dcb9f389c2a39f806f5fbb0ce67de90b26eb6
Cryofrostt <benzaaa35@hotmail.com>
2018-05-21 15:47:51 +0700

    321

--- recruit-status-reject-link.php
+++ recruit-status-reject-link.php
@@ -2,7 +2,7 @@
 include "dblink.php";
 $result = $conn->query("UPDATE recruitinfo
                         SET Status='สละสิทธิ์', MovedUniversityName='".$_POST['inUni']."'
-                        WHERE RecruitID='".$_GET['inID']."';");
+                        WHERE RecruitID='".$_POST['inID']."';");
 
 $conn->close();
 ?>
