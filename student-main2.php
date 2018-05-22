73d8b4e36c24a9077fb2b7e696aff320abf21113
darknessdearm <38848518+darknessdearm@users.noreply.github.com>
2018-05-22 17:17:04 +0700

    update ui

--- student-main2.php
+++ student-main2.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>สมัครโครงการ</title>
+    <title>ระบบสารสนเทศเพื่อการบริหารการศึกษา</title>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
@@ -20,7 +20,6 @@
 </head>
 <body>
     <div id="header" class= "header">
-            <a href="#home">หน้าหลัก</a>
             <a href="student-main.php">ข้อมูลนักศึกษา</a>
             <a class = "active" href="student-main2.php">ลงทะเบียนเรียน</a>
             <a href="student-main3.php">ตารางเรียน</a>
@@ -31,7 +30,6 @@
             <a href="student-home.php" class="logout">logout</a>
         </div>
     <div id="left">
-        
         <ul class="nav nav-pills nav-stacked" id="tab">
             <li><a data-toggle="tab" href="#menu1">ลงทะเบียนรายวิชา</a></li>
             <li><a data-toggle="tab" href="#menu2">เพิ่มรายวิชา</a></li>
@@ -40,8 +38,6 @@
         </ul>
      </div>
      <div id="main">
-        
-        
         <div class="tab-content" id="tab-content">
             <div id="menu1" class="tab-pane fade">
                 
