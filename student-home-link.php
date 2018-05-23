45d66e641d8a046e7c097831f9eeb576c9885028
Bank <saksitpz1011@gmail.com>
2018-05-23 15:04:33 +0700

    asd

--- student-home-link.php
+++ student-home-link.php
@@ -7,24 +7,24 @@
         $result = $conn->query("SELECT *
                                 FROM studentinfo
                                 WHERE StudentID = '$id' AND Password ='$pswd';");
-            if($id==NULL || $pswd==NULL){
-                $conn->close();
-                echo    '<script> alert("กรุณากรอกข้อมูลให้ครบทุกช่อง");
-                                window.location="student-home.php"; 
-                        </script>';
-            }
-            else if($result->num_rows == 1){
-                $conn->close();
-                $_SESSION['id'] = $id;
-                $_SESSION['pswd'] = $pswd;
-                $_SESSION['role'] = 'student';
-                header("Location:student-afterlogin-test.php?");   
-            }
-            else{
-                $conn->close();   
-                echo    '<script> alert("รหัสนักศึกษาและรหัสผ่านไม่ตรงกัน");
-                                window.location="student-home.php"; 
-                        </script>';
-            }
+        if($id==NULL || $pswd==NULL){
+            $conn->close();
+            echo    '<script> alert("กรุณากรอกข้อมูลให้ครบทุกช่อง");
+                            window.location="student-home.php"; 
+                    </script>';
+        }
+        else if($result->num_rows == 1){
+            $conn->close();
+            $_SESSION['id'] = $id;
+            $_SESSION['pswd'] = $pswd;
+            $_SESSION['role'] = 'student';
+            header("Location:student-main.php");   
+        }
+        else{
+            $conn->close();   
+            echo    '<script> alert("รหัสนักศึกษาและรหัสผ่านถูกต้อง");
+                            window.location="student-home.php"; 
+                    </script>';
+        }
     ?>
 </html>
