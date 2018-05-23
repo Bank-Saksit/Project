efacb7351325e4186e163873224ee31143f9daa6
darknessdearm <38848518+darknessdearm@users.noreply.github.com>
2018-05-24 01:24:48 +0700

    ddd

--- student-new-login.php
+++ student-new-login.php
@@ -10,99 +10,9 @@ session_start();
         <title>สำหรับนักศึกษาใหม่</title>
         <style>
             @import "global1.css";
-            html, body { 
-                margin: 0;
-                padding: 0;
-                background: #dfdfdf;
-                color: #444444;
-            }
-            #main {
-                width: 90%;
-                float: right;
-                position: relative;
-                top: 50px;
-            }
-    
-            #left {
-                float: left;
-                width: 10%;
-                position: relative;
-            }
-            #back {
-                margin: 20px;
-                font-weight: bold;
-            }
-            #header {
-                width: 100%;
-                position: relative;
-            }
-            #header > h1 {
-                font-size: 50px;
-                position: absolute;
-                bottom: 0;
-                width: 100%;
-                border-bottom: 5px solid;
-            }
-            div#left > a {
-                text-decoration: none;
-                margin: 5px;
-                color: #444444;
-            }
-            div#sub > a {
-                text-decoration: underline;
-                margin: 5px;
-                color: #ffffff;
-            }
-            div#content {
-                clear: both;
-                width: 100%;
-                position: relative;
-            }
-            #c-top {
-                width: 100%;
-                height: 360px;
-                top: 0;
-                text-align: left;
-                position: relative;
+            @import "temphome.css";
+            div#content{          
                 background-image: url(img/gallery/980x380/064.jpg);
-                color: white;
-            }
-            form{
-                padding-top : 5px;
-                padding-left : 30px;
-            }
-            input[type=text] {
-                background-color:rgba(0,0,0,0.8);
-                margin-top: 10px;
-                padding-left: 30px;
-                width: 310px;
-                height:30px;
-                text-align: left;
-                color: white;
-            }
-            input[type=password] {
-                background-color:rgba(0,0,0,0.8);
-                margin-top: 20px;
-                padding-left: 30px;
-                width: 310px;
-                height: 30px;
-                text-align: left;
-                color: white;
-            }
-            ::placeholder {
-                color: white;
-            }
-            input[type=submit] {
-                width: 60px;
-                margin-top: 25px;
-                margin-left: 10px;
-                background: white;
-            }
-            div#text >a {
-                text-decoration: underline;
-            }
-            #text{
-                padding-left : 30px;
             }
         </style>    
 
@@ -115,7 +25,7 @@ session_start();
         }
         ?> 
         <div id="left">
-            <br><a href="student-home.php" id="back">< back</a>
+            <br><a href="#" id="back"></a>
         </div>
         <div id="main">
             <div id="header">
@@ -127,7 +37,9 @@ session_start();
                         <h1>ระบบสารสนเทศ<br>เพื่อการบริหารการศึกษา</h1>
                         <input type="text" name="id" placeholder="รหัสนักศึกษา"><br>
                         <input type="password" name="pswd" placeholder="รหัสบัตรประจำตัวประชาชน"><br>
-                        <input type="submit" value="เข้าสู่ระบบ">
+                        <div id = "sub">
+                            <input type="submit" value="เข้าสู่ระบบ">
+                        </div>
                     </form>
                 </div>
             </div>
