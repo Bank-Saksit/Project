b7a15264e41c2f745e85b736e58dcd5b6e1fb365
Cryofrostt <benzaaa35@hotmail.com>
2018-05-24 17:19:57 +0700

    หน้าลงทะเบียนเรียนเชื่อมเสร็จแล้ว(เหลือเพิ่มแสดงรายละเอียด)

--- student-main2-link.php
+++ student-main2-link.php
@@ -1,17 +1,17 @@
 <?php
 include "dblink.php";
 if( $_GET['type']=='01' ){
-    $result = $conn->query("SELECT s.StudentID
-                            FROM studentinfo s
-                            WHERE s.StudentID = '".$_GET['inID']."';");
+    $result = $conn->query("SELECT s.StudentID, ss.SubjectSectionID, se.SubjectID, se.SectionNumber
+                            FROM studentinfo s, student_subject ss, sectioninfo se
+                            WHERE s.StudentID=ss.StudentID AND ss.SubjectSectionID=se.SubjectSectionID AND s.StudentID = '".$_GET['inID']."';");
 
     $outp = "[";
     while($rs = $result->fetch_array(MYSQLI_ASSOC)) {
         if ($outp != "[") {$outp .= ",";}
-        $outp .= '{"StudentID":"'.$rs["StudentID"].'"}';
-        // $outp .= '"Department":"'.$rs["Department"].'",';
-        // $outp .= '"Degree":"'.$rs["Degree"].'",';
-        // $outp .= '"Course":"'.$rs["Course"].'",';
+        $outp .= '{"StudentID":"'.$rs["StudentID"].'",';
+        $outp .= '"SubjectSectionID":"'.$rs["SubjectSectionID"].'",';
+        $outp .= '"SubjectID":"'.$rs["SubjectID"].'",';
+        $outp .= '"SectionNumber":"'.$rs["SectionNumber"].'"}';
         // $outp .= '"MobileNumber":"'.$rs["MobileNumber"].'",';
         // $outp .= '"TelNumber":"'.$rs["TelNumber"].'",';
         // $outp .= '"Email":"'.$rs["Email"].'",';
@@ -94,9 +94,36 @@ elseif( $_GET['type']=='11' ){
                     WHERE SubjectID='".$_GET['sub5']."' AND SectionNumber=".$_GET['sec5']);
 }
 elseif( $_GET['type']=='12' ){
-    $conn->query("UPDATE studentinfo
-                SET Address='".$_GET['inAdd']."', Province='".$_GET['inPr']."', Postcode='".$_GET['inPo']."', MobileNumber='".$_GET['inMN']."', TelNumber='".$_GET['inTN']."'
-                WHERE StudentID='".$_GET['inID']."';");
+    if( $_GET['sub0']!='' )
+        $conn->query("INSERT INTO student_subject
+                    SELECT SubjectSectionID, ".$_GET['inID'].", 0
+                    FROM sectioninfo
+                    WHERE SubjectID='".$_GET['sub0']."' AND SectionNumber=".$_GET['sec0']);
+    if( $_GET['sub1']!='' )
+        $conn->query("INSERT INTO student_subject
+                    SELECT SubjectSectionID, ".$_GET['inID'].", 0
+                    FROM sectioninfo
+                    WHERE SubjectID='".$_GET['sub1']."' AND SectionNumber=".$_GET['sec1']);
+    if( $_GET['sub2']!='' )
+        $conn->query("INSERT INTO student_subject
+                    SELECT SubjectSectionID, ".$_GET['inID'].", 0
+                    FROM sectioninfo
+                    WHERE SubjectID='".$_GET['sub2']."' AND SectionNumber=".$_GET['sec2']);
+}
+elseif( $_GET['type']=='13' ){
+    $conn->query("UPDATE student_subject
+                SET SubjectSectionID=(SELECT SubjectSectionID
+                                    FROM sectioninfo
+                                    WHERE SubjectID='".$_GET['inSub']."' AND SectionNumber=".$_GET['inSec'].")
+                WHERE StudentID='".$_GET['inID']."' AND SubjectSectionID IN (SELECT SubjectSectionID
+                                                                FROM sectioninfo
+                                                                WHERE SubjectID='".$_GET['inSub']."' )");
+}
+elseif( $_GET['type']=='14' ){
+    $conn->query( "DELETE FROM student_subject
+                WHERE StudentID='".$_GET['inID']."' AND SubjectSectionID IN (SELECT SubjectSectionID
+                                                                            FROM sectioninfo
+                                                                            WHERE SubjectID='".$_GET['inSub']."')" );
 }
 
 $conn->close();
--- student-main2.php
+++ student-main2.php
@@ -52,7 +52,7 @@
                             echo "<option value = '" . $row['SubjectID']."'>".$row['SubjectID']."</option>";
                         }
                         echo"</select>";
-                        echo" Section <select name = \"Section[]\" class='section'>";
+                        echo" กลุ่ม <select name = \"Section[]\" class='section'>";
                         echo"<option value = 01 >1</option>";
                         echo"<option value = 02 >2</option>";
                         echo"<option value = 03 >3</option>";
@@ -63,10 +63,32 @@
                     <div id="demo"></div><br>
                     <input type="button" value="ยืนยัน" onclick="update1()">
                 </form>
-				<div id="alert"></div>
-				<div id="alert1"></div>
             </div>
-            <div id="menu2" class="tab-pane fade"></div>
+            <div id="menu2" class="tab-pane fade">
+                <h3>เพิ่มวิชา</h3>
+                <form>
+                    <span>วิชาที่ 1 </span>
+                    <?php
+                        include "dblink.php";
+                        $result = mysqli_query($conn,"SELECT * FROM subjectinfo");
+                        echo"<select name = 'Subject2[]' class='subject2'>";
+                        echo"<option value = ''>โปรดเลือก</option>";
+                        while($row = mysqli_fetch_array($result)){
+                            echo "<option value = '" . $row['SubjectID']."'>".$row['SubjectID']."</option>";
+                        }
+                        echo"</select>";
+                        echo" กลุ่ม <select name = \"Section2[]\" class='section2'>";
+                        echo"<option value = 01 >1</option>";
+                        echo"<option value = 02 >2</option>";
+                        echo"<option value = 03 >3</option>";
+                        echo"</select>";
+                    ?>
+                    <button type="button" id="add2" class="check2" >+</button>
+                    <button type="button" id="remove2" class="check2" >-</button>
+                    <div id="demo2"></div><br>
+                    <input type="button" value="ยืนยัน" onclick="update2()">
+                </form>
+            </div>
             <div id="menu3" class="tab-pane fade"></div>
             <div id="menu4" class="tab-pane fade"></div>
         </div>
@@ -86,7 +108,7 @@
 
             function load(){
                 var xmlhttp = new XMLHttpRequest();
-                var url = location.protocol+'//'+location.host+"/Project/student-main2-link.php?type=01&inID=59070501066";
+                var url = location.protocol+'//'+location.host+"/Project/student-main2-link.php?type=01&inID=59070501083";
                 
                 xmlhttp.onreadystatechange=function() {
                     if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
@@ -99,6 +121,53 @@
 
             function display(response) {
                 arr = JSON.parse(response);
+
+                var out3 = "<h3>ย้ายกลุ่ม</h3>"+
+                        "<form>"+
+                        "<p>วิชา: <select id='inSub3' onchange='change3()'>";
+                for( var i=0 ; i<arr.length ; i++ )
+                    out3+="<option value='"+arr[i].SubjectID+"'>"+arr[i].SubjectID+"</option>";
+                out3+="</select> กลุ่ม <select id='inSec3'>"+
+                        "<option value=01>1</option>"+
+                        "<option value=02>2</option>"+
+                        "<option value=03>3</option>"+
+                        "</select>"+
+                        "<br><br><input type='button' value='ยืนยัน' onclick='update3()'>"+
+                        "</form>";
+                document.getElementById("menu3").innerHTML = out3;
+
+                var out4 = "<h3>ลดรายวิชา</h3>"+
+                        "<form>"+
+                        "<p>วิชา: <select id='inSub4' onchange='change4()'>";
+                for( var i=0 ; i<arr.length ; i++ )
+                    out4+="<option value='"+arr[i].SubjectID+"'>"+arr[i].SubjectID+"</option>";
+                out4+="</select> <div id='outSec4'></div>"+
+                        "<br><br><input type='button' value='ยืนยัน' onclick='update4()'>"+
+                        "</form>";
+                document.getElementById("menu4").innerHTML = out4;
+
+                change3();
+                change4();
+            }
+
+            function change3(){
+                var tmp = document.getElementById('inSub3').value;
+                for( var i=0 ; i<arr.length ; i++ ){
+                    if( tmp==arr[i].SubjectID ){
+                        document.getElementById('inSec3').selectedIndex = arr[i].SectionNumber-1;
+                        break;
+                    }
+                }
+            }
+
+            function change4(){
+                var tmp = document.getElementById('inSub4').value;
+                for( var i=0 ; i<arr.length ; i++ ){
+                    if( tmp==arr[i].SubjectID ){
+                        document.getElementById('outSec4').innerHTML = "กลุ่ม "+arr[i].SectionNumber;
+                        break;
+                    }
+                }
             }
 
             function update1(){
@@ -122,61 +191,59 @@
 
             function update2(){
                 var xmlhttp = new XMLHttpRequest();
-                var inAdd = document.getElementById('inAdd').value;
-                var inPr = document.getElementById('inPr').value;
-                var inPo = document.getElementById('inPo').value;
-                var inMN = document.getElementById('inMN').value;
-                var inTN = document.getElementById('inTN').value;
-                var url = location.protocol + '//' + location.host+"/Project/student-profile-link.php?type=12&inID="+arr[0].StudentID+
-                    "&inAdd="+inAdd+"&inPr="+inPr+"&inPo="+inPo+"&inMN="+inMN+"&inTN="+inTN;
+                var sub = document.getElementsByClassName('subject2');
+                var sec = document.getElementsByClassName('section2');
+                var url = location.protocol + '//' + location.host+"/Project/student-main2-link.php?type=12&inID="+arr[0].StudentID;
+                    for( var i=0 ; i<3 ; i++ ){
+                        if( sub[i] && sub[i].value!='' ) url+="&sub"+i+"="+sub[i].value+"&sec"+i+"="+sec[i].value;
+                        else url+="&sub"+i+"=''&sec"+i+"=''";
+                    }
 
                 xmlhttp.onreadystatechange=function() {
                     if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
-                        document.getElementById("res2").innerHTML = "แก้ไขสำเร็จ";
+
                     }
                 }
                 xmlhttp.open("GET", url, true);
                 xmlhttp.send();
             }
 
-            /*function update3(){
+            function update3(){
                 var xmlhttp = new XMLHttpRequest();
-                var inRela = document.getElementById('inRela').value;
-                var inpMN = document.getElementById('inpMN').value;
-                var inpTN = document.getElementById('inpTN').value;
-                var inpEm = document.getElementById('inpEm').value;
-                var inpID = document.getElementById('inpID').value;
-                var inpPre = document.getElementById('inpPre').value;
-                var inpFn = document.getElementById('inpFn').value;
-                var inpLn = document.getElementById('inpLn').value;
-                var inpGe = document.getElementById('inpGe').value;
-                var inpDOB = document.getElementById('inpDOB').value;
-                var inpNa = document.getElementById('inpNa').value;
-                var inpRa = document.getElementById('inpRa').value;
-                var inpRe = document.getElementById('inpRe').value;
-                var inpBl = document.getElementById('inpBl').value;
-                var inpAdd = document.getElementById('inpAdd').value;
-                var inpPr = document.getElementById('inpPr').value;
-                var inpPo = document.getElementById('inpPo').value;
-                var url = location.protocol + '//' + location.host+"/Project/student-profile-link.php?type=13&inID="+arr[0].StudentID+
-                    "&inRela="+inRela+"&inpMN="+inpMN+"&inpTN="+inpTN+"&inpEm="+inpEm+"&inpID="+inpID+"&inpPre="+inpPre+"&inpFn="+inpFn+
-                    "&inpLn="+inpLn+"&inpGe="+inpGe+"&inpDOB="+inpDOB+"&inpNa="+inpNa+"&inpRa="+inpRa+"&inpRe="+inpRe+"&inpBl="+inpBl+
-                    "&inpAdd="+inpAdd+"&inpPr="+inpPr+"&inpPo="+inpPo;
+                var sub = document.getElementById('inSub3').value;
+                var sec = document.getElementById('inSec3').value;
+                var url = location.protocol + '//' + location.host+"/Project/student-main2-link.php?type=13&inID="+arr[0].StudentID;
+                    url+="&inSub="+sub+"&inSec="+sec;
 
                 xmlhttp.onreadystatechange=function() {
                     if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
-                        document.getElementById("res3").innerHTML = "แก้ไขสำเร็จ";
+
                     }
                 }
                 xmlhttp.open("GET", url, true);
                 xmlhttp.send();
-            }*/
+            }
+
+            function update4(){
+                var xmlhttp = new XMLHttpRequest();
+                var sub = document.getElementById('inSub4').value;
+                var url = location.protocol + '//' + location.host+"/Project/student-main2-link.php?type=14&inID="+arr[0].StudentID;
+                    url+="&inSub="+sub;
+
+                xmlhttp.onreadystatechange=function() {
+                    if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+
+                    }
+                }
+                xmlhttp.open("GET", url, true);
+                xmlhttp.send();
+            }
 
             $(function(){
-                n = 2;
+                var n = 2;
                 if(n==2){	$('button[id="remove"]').prop('disabled', true);	}
                 else{	$('button[id="remove"]').prop('disabled', false);	}
-                if(n==5){	$('button[id="add"]').prop('disabled', true);	}
+                if(n==7){	$('button[id="add"]').prop('disabled', true);	}
                 else{	$('button[id="add"]').prop('disabled', false);	}
                 
                 <?php
@@ -188,7 +255,7 @@
                         echo "'<option value = \"" . $row['SubjectID']."\">".$row['SubjectID']."</option>'+";
                     }
                     echo"'</select>'+";
-                    echo" ' Section <select name = \"Section[]\" class=\"section\">'+ ";
+                    echo" ' กลุ่ม <select name = \"Section[]\" class=\"section\">'+ ";
                     echo" '<option value = 1 >1</option>'+ ";
                     echo" '<option value = 2 >2</option>'+ ";
                     echo" '<option value = 3 >3</option>'+ ";
@@ -213,6 +280,50 @@
                     else{	$('button[id="remove"]').prop('disabled', false);	}
                     if(n==7){	$('button[id="add"]').prop('disabled', true);	}
                     else{	$('button[id="add"]').prop('disabled', false);	}
+                })	
+            })
+
+            $(function(){
+                var n = 2;
+                if(n==2){	$('button[id="remove2"]').prop('disabled', true);	}
+                else{	$('button[id="remove2"]').prop('disabled', false);	}
+                if(n==4){	$('button[id="add2"]').prop('disabled', true);	}
+                else{	$('button[id="add2"]').prop('disabled', false);	}
+                
+                <?php
+                    $result = mysqli_query($conn,"SELECT * FROM subjectinfo");
+
+                    echo"var input = '<select name = \"Subject2[]\" class=\"subject2\">'+";
+                    echo"'<option value = \"\">โปรดเลือก</option>'+";
+                    while($row = mysqli_fetch_array($result)){
+                        echo "'<option value = \"" . $row['SubjectID']."\">".$row['SubjectID']."</option>'+";
+                    }
+                    echo"'</select>'+";
+                    echo" ' กลุ่ม <select name = \"Section2[]\" class=\"section2\">'+ ";
+                    echo" '<option value = 1 >1</option>'+ ";
+                    echo" '<option value = 2 >2</option>'+ ";
+                    echo" '<option value = 3 >3</option>'+ ";
+                    echo"'</select>'";
+                ?>	
+
+                $('#add2').click(function(){
+                    $("#demo2").append('<span>วิชาที่' +' '+ n +' '+input+'</span>'+ '<br>' );
+                    n++;
+                    if(n==2){	$('button[id="remove2"]').prop('disabled', true);	}
+                    else{	$('button[id="remove2"]').prop('disabled', false);	}
+                    if(n==4){	$('button[id="add2"]').prop('disabled', true);	}
+                    else{	$('button[id="add2"]').prop('disabled', false);	}
+                })
+                $('#remove2').click(function(){
+                    $('#demo2 > select[name^=Subject2]:last').remove();
+                    $('#demo2 > select[name^=Section2]:last').remove();
+                    $("#demo2 > br:last").remove();
+                    $("span:last-child").remove();
+                    n--;
+                    if(n==2){	$('button[id="remove2"]').prop('disabled', true);	}
+                    else{	$('button[id="remove2"]').prop('disabled', false);	}
+                    if(n==4){	$('button[id="add2"]').prop('disabled', true);	}
+                    else{	$('button[id="add2"]').prop('disabled', false);	}
                 })	
             })
         </script>
