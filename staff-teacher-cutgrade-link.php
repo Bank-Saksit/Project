270fba0a9c0f71cdd1447d5af8193d4d3d77ffd2
TarZz <guitar_sk555@hotmail.com>
2018-05-25 20:04:12 +0700

    as

---
+++ staff-teacher-cutgrade-link.php
@@ -0,0 +1,57 @@
+<?php 
+include "dblink.php";
+$id=(int)$_GET['inID'];
+$sub=mysqli_real_escape_string($conn,$_GET['sub']);
+    if( $_GET['type']=='01' ){
+        $result = $conn->query("SELECT sc.SubjectID, sc.SectionNumber,su.SubjectName,t.SubjectSectionID
+                                FROM teachersec t,sectioninfo sc,subjectinfo su
+                                WHERE   t.SubjectSectionID=sc.SubjectSectionID AND 
+                                        sc.SubjectID = su.SubjectID AND
+                                        sc.Semester = '1' AND
+                                        sc.AcademicYear = '2018'AND
+                                        t.StaffID = $id;");
+            
+            $outp = "[";
+            while($rs = $result->fetch_array(MYSQLI_ASSOC)) {
+                if ($outp != "[") {$outp .= ",";}
+                $outp .= '{"SubjectID":"'.$rs["SubjectID"].'",';
+                $outp .= '"SectionNumber":"'.$rs["SectionNumber"].'",';
+                $outp .= '"SubjectSectionID":"'.$rs["SubjectSectionID"].'",';
+                $outp .= '"SubjectName":"'.$rs["SubjectName"].'"}';
+            }
+            $outp .="]";
+            echo($outp);    
+    }
+    else if( $_GET['type']=='02' ){
+        $result = $conn->query("SELECT ss.StudentID,st.FirstName,st.LastName
+                                FROM teachersec t,sectioninfo sc,student_subject ss,subjectinfo su,studentinfo st
+                                WHERE   t.SubjectSectionID=ss.SubjectSectionID AND 
+                                        t.SubjectSectionID=sc.SubjectSectionID AND 
+                                        sc.SubjectID = su.SubjectID AND
+                                        sc.Semester = '1' AND
+                                        sc.AcademicYear = '2018'AND
+                                        st.StudentID = ss.StudentID AND
+                                        t.StaffID = $id AND 
+                                        ss.SubjectSectionID = $sub;");
+        
+            $outp = "[";
+            while($rs = $result->fetch_array(MYSQLI_ASSOC)) {
+                if ($outp != "[") {$outp .= ",";}
+                $outp .= '{"StudentID":"'.$rs["StudentID"].'",';
+                $outp .= '"FirstName":"'.$rs["FirstName"].'",';
+                $outp .= '"LastName":"'.$rs["LastName"].'"}';
+            }
+            $outp .="]";
+            echo($outp);    
+    }
+    else if( $_GET['type']=='03' ){
+        if($_GET['gpa']!=-1){
+            $gpa=floatval($_GET['gpa']);
+            $sid=mysqli_real_escape_string($conn,$_GET['SID']);
+            $result = $conn->query("UPDATE student_subject
+                                    SET GPA= $gpa
+                                    WHERE StudentID='$sid'");
+        }
+    }
+
+?>
--- staff-teacher-main3.php
+++ staff-teacher-main3.php
@@ -38,7 +38,7 @@
             <a href="javascript:void(0);" class="icon" onclick="myFunction()">
                 <i class="fa fa-bars"></i>
             </a>
-            <a href="staff-home.php" class="logout">ออกจากระบบ</a>
+            <a href="staff-logout.php" class="logout">ออกจากระบบ</a>
     </div>
    <div id="left">
         <ul class="nav nav-pills nav-stacked" id="tab">
@@ -60,14 +60,100 @@
             </div>
         </div>
         <script>
-        function myFunction() {
-            var x = document.getElementById("top");
-            if (x.className === "top") {
-                x.className += " responsive";
+            load();
+
+            function myFunction() {
+                var x = document.getElementById("top");
+                if (x.className === "top") {
+                    x.className += " responsive";
                 } 
-            else {
-                x.className = "top";
+                else {
+                    x.className = "top";
                 }
+            }
+
+            function load(){
+                var xmlhttp = new XMLHttpRequest();
+                var url = location.protocol+'//'+location.host+"/Project/staff-teacher-cutgrade-link.php?type=01&inID="+<?php echo $_SESSION['id'];?>+"&sub=0"+"&gpa=''&SID=''";
+                    
+                xmlhttp.onreadystatechange=function() {
+                    if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+                        display(xmlhttp.responseText);
+                    }
+                }
+                xmlhttp.open("GET", url, true);
+                xmlhttp.send();
+            }
+
+            function display(response){
+                var arr = JSON.parse(response);
+                var out1 = "<form> รายวิชาที่ต้องการตัดเกรด: <select id='sub'>";
+                        for(i=0;i<arr.length;i++){
+                            out1+="<option value="+arr[i].SubjectSectionID+">"+arr[i].SubjectID+"&nbspsec:"+arr[i].SectionNumber+"&nbsp"+arr[i].SubjectName+"</option>";
+                        }
+                    out1 += "</select><br>"+
+                            "<br><input type='button' value='เลือก' id='edit1' onclick='selectSub()'>"+
+                            "</form>";
+                    document.getElementById("menu1").innerHTML = out1;
+            }
+
+            function selectSub(){
+                var xmlhttp = new XMLHttpRequest();
+                var sub = document.getElementById('sub').value;
+                var url = location.protocol+'//'+location.host+"/Project/staff-teacher-cutgrade-link.php?type=02&inID="+<?php echo $_SESSION['id'];?>+"&sub="+sub+"&gpa=''&SID=''";
+                    
+                xmlhttp.onreadystatechange=function() {
+                    if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+                        disGrade(xmlhttp.responseText);
+                    }
+                }
+                xmlhttp.open("GET", url, true);
+                xmlhttp.send();
+            }
+
+            function disGrade(response){
+                var arr = JSON.parse(response);
+                var out1 = "<form>";
+                    for(i=0;i<arr.length;i++){
+                        out1+=  arr[i].StudentID+"&nbsp"+
+                                arr[i].FirstName+"&nbsp"+
+                                arr[i].LastName+"&nbsp"+
+                                "<select class='GPA'>"+
+                                "<option value=-1>-</option>"+
+                                "<option value=4.00>A</option>"+
+                                "<option value=3.50>B+</option>"+
+                                "<option value=3.00>B</option>"+
+                                "<option value=2.50>C+</option>"+
+                                "<option value=2.00>C</option>"+
+                                "<option value=1.50>D+</option>"+
+                                "<option value=1.00>D</option>"+
+                                "<option value=0.00>F</option>"+
+                                "</select><br>";
+                    }
+                    out1+= "<br><input type='button' value='ยืนยัน' onclick='Grade("+JSON.stringify(response)+")'></form>"
+                    document.getElementById("menu1").innerHTML = out1;
+            }
+            function Grade(response){
+                var arr = JSON.parse(response);
+                var x = document.getElementsByClassName("GPA");
+                for(i=0;i<x.length;i++){
+                    sendGrade(x[i].value,arr[i].StudentID);
+                }
+                alert("ตัดเกรดเรียบร้อยแล้ว");
+                load();
+
+            }
+            function sendGrade(gpa,SID){
+                var xmlhttp = new XMLHttpRequest();
+                var url = location.protocol+'//'+location.host+"/Project/staff-teacher-cutgrade-link.php?type=03&inID="+<?php echo $_SESSION['id'];?>+"&sub=0"+"&gpa="+gpa+"&SID="+SID;
+                    
+                xmlhttp.onreadystatechange=function() {
+                    if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+                        
+                    }
+                }
+                xmlhttp.open("GET", url, true);
+                xmlhttp.send();
             }
         </script>
      </div>
