1e9b4418df9f93a63bb37548972a1f8b4c06a37d
TarZz <guitar_sk555@hotmail.com>
2018-05-26 23:03:39 +0700

    qeqweqe

---
+++ staff-newpw.php
@@ -0,0 +1,61 @@
+<?php
+session_start();
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <title>แก้ไขรหัสผ่าน</title>
+    <style>
+        @import "global1.css";
+        @import "temphome.css";
+            div#content{          
+                background-image: url(img/gallery/980x380/065.jpg);
+            }
+    </style>
+</head>
+<body>
+    <?php
+        if(!isset($_SESSION['id'])||!isset($_SESSION['idcard'])||$_SESSION['role']!='staff'){
+            header("location:staff-forgetpassword.php");
+        }
+    ?>
+    <div id="left">
+        <br><a href="staff-logout.php" id="back"></a>
+        <!-- <a href="staff-new-logout.php" class="btn btn-info btn-lg" id = "back">
+                <span class="glyphicon glyphicon-chevron-left"></span> 
+            </a> -->
+    </div>
+    <div id="main">
+        <div id="header">
+             <h1>สร้างรหัสผ่านใหม่</h1>
+        </div>
+        <div id="content">
+            <div id="c-left">
+                <form method="post" action ="staff-update-pw.php" >
+                        <h1>สร้างรหัสผ่านใหม่</h1>
+                        <div id = "c-in">
+                            <input type="password" name="pw" placeholder="รหัสผ่านใหม่"><br>
+                            <input type="password" name="pw2" placeholder="ยืนยันรหัสผ่านใหม่"><br>
+                            <div id = "sub">
+                                <input type="submit" value="ยืนยัน">
+                                <input type="button" id="cancle" onclick ="window.location.href='staff-logout.php'" value = "ยกเลิก">
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            <div id="c-right">
+                
+                
+            </div>
+        </div>
+         <?php include "recruit-footer.php"; ?>
+    </div>    
+    
+</body>
+</html>
--- staff-update-pw.php
+++ staff-update-pw.php
@@ -15,6 +15,7 @@
     include "dblink.php";
     $pw=mysqli_real_escape_string($conn,$_POST['pw']);
     $pw2=mysqli_real_escape_string($conn,$_POST['pw2']);
+    $enpw = md5(md5(md5(base64_encode($pw))));
     $id=$_SESSION['id'];
         if($pw==NULL || $pw2 ==NULL){
             echo    "<script>
@@ -34,7 +35,7 @@
                     // </script>';
         }
         else if($pw==$pw2){
-            $conn->query("UPDATE staffinfo  SET Password = '$pw' WHERE StaffID = $id ;");
+            $conn->query("UPDATE staffinfo  SET Password = '$enpw' WHERE StaffID = $id ;");
             $conn->close();
             session_destroy();
             echo    "<script>
