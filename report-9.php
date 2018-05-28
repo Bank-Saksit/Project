aab0f704586e379ecbabcd08c76edd40a5363244
darknessdearm <38848518+darknessdearm@users.noreply.github.com>
2018-05-28 17:09:01 +0700

    safa

--- report-9.php
+++ report-9.php
@@ -7,10 +7,10 @@
                                     ss.GPA = -1
                             GROUP BY su.SubjectName LIMIT 5;");
     $out = "<h2>วิชาที่มีการถอนสูงสุด 5 อันดับแรก</h2>";
-    $out .= "<table><tr><td id = \'t\' align = \'center\'>รายวิชา</td><td id = \'t\' align = \'center\'>จำนวน</td></tr>";
+    $out .= "<table><tr><td id = 't' align = 'center'>รายวิชา</td><td id = 't' align = 'center'>จำนวน</td></tr>";
     $rs1=mysqli_fetch_all($result,MYSQLI_ASSOC);
     for($i=0;$i<$result->num_rows;$i++){
-        $out .="<tr><td id = \'t\' align = \'center\'>".$rs1[$i]['SubjectID']."&nbsp".$rs1[$i]['SubjectName']."</td><td id = \'t\' align = \'center\'>".$rs1[$i]['NUM']."</td>";
+        $out .="<tr><td id = 't' align = 'center'>".$rs1[$i]['SubjectID']."&nbsp".$rs1[$i]['SubjectName']."</td><td id = 't' align = 'center'>".$rs1[$i]['NUM']."</td>";
     }
     $out .="</table>";
     echo $out;          
