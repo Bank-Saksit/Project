c11a3dd4b55bcd6e275360d773b95e277d67d17f
MMandMeiei <ming2541@hotmail.com>
2018-05-28 19:59:38 +0700

    l

---
+++ insert data.php
@@ -0,0 +1,11 @@
+//insert to database
+INSERT INTO subjectinfo VALUES ('GEN101','Physical Education','ก่อนที่จะมีการประกาศอย่างเป็นทางการเกี่ยวกับการจัดตั้งวงบีเอ็นเคโฟร์ตีเอตในประเทศไทย เมื่อปี พ.ศ. 2554','3');
+INSERT INTO subjectinfo VALUES ('GEN111','Man and Ethics of Living',' ยะซุชิ อะกิโมะโตะ ผู้ก่อตั้งแฟรนไชส์เอเคบีโฟร์ตีเอต ได้ให้สัมภาษณ์กับนิตยสารจีคิว (GQ)','3');
+INSERT INTO subjectinfo VALUES ('GEN121','Learning and Problem Solving Skills','และรายการทีวี วะรัตเตะ อีโตะโมะ ของประเทศญี่ปุ่นถึงความเป็นไปได้ในการจัดตั้งวงน้องสาวนอกประเทศญี่ปุ่นต่อจากวงเจเคทีโฟร์ตีเอต (JKT48)','3');
+INSERT INTO subjectinfo VALUES ('GEN231','Miracle of Thinking','ซึ่งประเทศที่เป็นไปได้ก็คือประเทศไต้หวันและประเทศไทย','3');
+INSERT INTO subjectinfo VALUES ('GEN241','Beauty of Life','ช่วงปลายปี พ.ศ. 2556 โรส เอ็นเตอร์เทนเมนท์ แพ้การประมูลทีวีดิจิทัล หมวดหมู่เด็ก เยาวชน และครอบครัว ของกสทช','3');
+INSERT INTO subjectinfo VALUES ('GEN351','Modern Management and Leadership',' ทำให้เดินหน้าทำงานเกี่ยวกับดูแลศิลปิน จิรัฐ บวรวัฒนะ ประธานเจ้าหน้าที่บริหาร บริษัท โรส อาร์ทิสท์ แมเนจเม้นท์','3');
+INSERT INTO subjectinfo VALUES ('GEN352','Defense of the Ancients','ได้ติดต่อ เอเคเอส บริษัทที่จัดการและบริหารกลุ่มไอดอลญี่ปุ่น เอเคบีโฟร์ตีเอต เพื่อขอสิทธิ์มาทำวงน้องสาวในประเทศไทย','3');
+INSERT INTO subjectinfo VALUES ('GEN451','Heroes of Newerth','ต่อมาเมื่อวันที่ 26 มีนาคม พ.ศ. 2559 ในงานคอนเสิร์ตจบการศึกษาของ มินะมิ ทะกะฮะชิ สมาชิกวงเอเคบีโฟร์ตีเอต','3');
+INSERT INTO subjectinfo VALUES ('GEN501','League of Legends','ได้มีการประกาศอย่างเป็นทางการถึงการก่อตั้งวงน้องสาวต่างประเทศของเอเคบีโฟร์ตีเอตวงใหม่อีกสามวงด้วยกัน ','3');
+INSERT INTO subjectinfo VALUES ('GEN541','Realm Of Valor','ซึ่งได้แก่วงทีพีอีโฟร์ตีเอต (TPE48) ประเทศไต้หวัน, เอ็มเอ็นแอลโฟร์ตีเอต (MNL48) ประเทศฟิลิปปินส์ และบีเอ็นเคโฟร์ตีเอต (BNK48) ประเทศไทย','3');
