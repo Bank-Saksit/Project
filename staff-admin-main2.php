b6b9a48445e3050b6a191f2f26382c9e32d114d3
TarZz <guitar_sk555@hotmail.com>
2018-05-28 16:31:03 +0700

    sfdgfg

--- staff-admin-main2.php
+++ staff-admin-main2.php
@@ -1,3 +1,6 @@
+<?php
+session_start();
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -39,6 +42,15 @@
     
 </head>
 <body>
+        <?php 
+            if(isset($_SESSION['id5']) && isset($_SESSION['pswd5']) && $_SESSION['role5'] == 'Admin') {
+                
+            }
+            else{
+                header("location: staff-home.php");
+                exit('</body></html>');
+            }
+        ?>
     <div class="top" id="top">
             <a href="staff-admin-main.php">นักเรียนสอบเข้าโครงการ</a>
             <a class = "active" href="staff-admin-main2.php">นักศึกษา</a>
